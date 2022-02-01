aabc3da9a0581567e8dd2d2871a8e664cef8de4f
João Vitor Duarte Mariucio <joao.mariucio@mesainc.com.br>
2022-02-01 09:52:05 -0300

    Initial Commit

---
+++ combine-swiftui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  combine-swiftui
+//
+//  Created by João Vitor Duarte Mariucio on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ combine-swiftuiUITests/combine_swiftuiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  combine_swiftuiUITestsLaunchTests.swift
+//  combine-swiftuiUITests
+//
+//  Created by João Vitor Duarte Mariucio on 01/02/22.
+//
+
+import XCTest
+
+class combine_swiftuiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
